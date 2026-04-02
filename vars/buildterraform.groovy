ccdd1850a37af329048893d6716d7b56d67ddd7f
polaristechverse <polaristechverse@gmail.com>
2026-04-02 10:35:07 +0530

    Create buildterraform.groovy

---
+++ vars/buildterraform.groovy
@@ -0,0 +1,12 @@
+
+    stage('Terraform Build') {
+        echo "Building the infra with terraform"
+        sh '''
+                terraform version
+                terraform init
+                terraform fmt
+                terraform validate
+                terraform plan 
+                terraform apply --auto-approve
+                '''
+    }
